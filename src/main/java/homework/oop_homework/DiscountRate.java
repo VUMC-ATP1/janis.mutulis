5ffc82d6c95b08a233f636bfa45bcf377e14b48e
“jmutulis” <janis.mutulis@gmail.com>
2022-07-09 12:40:44 +0300

    [feature/java]: java OOP homework

---
+++ src/main/java/homework/oop_homework/DiscountRate.java
@@ -0,0 +1,28 @@
+package homework.oop_homework;
+
+public class DiscountRate {
+    int levelPremium = 20;
+    int levelGold = 15;
+    int levelSilver = 10;
+    int noLevel = 0;
+
+    public DiscountRate() {
+    }
+
+    public int getLevelPremium() {
+        return levelPremium;
+    }
+
+    public int getLevelGold() {
+        return levelGold;
+    }
+
+    public int getLevelSilver() {
+        return levelSilver;
+    }
+
+    public int getNoLevel() {
+        return noLevel;
+    }
+
+}
